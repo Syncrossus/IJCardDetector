303ed270fe01f608d31df77fc57e39a28ea63dd8
Maudulo <ma.be92@hotmail.fr>
2016-04-23 11:50:25 +0200

    amélioration de la rotation
    + rotation améliorée
    + correction des derniers bugs sur cette fonction

---
+++ src/rotation/Rotation.java
@@ -0,0 +1,45 @@
+package rotation;
+
+import ij.process.ByteProcessor;
+import ij.process.ImageProcessor;
+
+public class Rotation {
+
+	/**
+	 * 
+	 * @param ip image to rotate
+	 * @param theta the angle to rotate the image
+	 * This function rotates a card with the equation of a line
+	 */
+	public static ImageProcessor rotate(ImageProcessor ip, int angle){
+
+		ImageProcessor ipRotate = new ByteProcessor(ip.getWidth(), ip.getHeight());
+
+		//calcul de l'angle en radian
+		double theta =  - angle/(180.0/Math.PI); 
+
+		//tourne l'image par rapport au centre de l'image d'origine
+		int centerX = ip.getWidth()/2;
+		int centerY = ip.getHeight()/2;
+
+		for(int i=0; i<ipRotate.getWidth();i++){
+			for(int j=0;j<ipRotate.getHeight();j++){
+
+				//récupère les anciennes coordonnées du pixel concerné
+				int coordX = (int)((i-centerX)*Math.cos(theta) - (j-centerY)*Math.sin(theta)+centerX); 
+				int coordY = (int)((i-centerX)*Math.sin(theta) + (j-centerY)*Math.cos(theta)+centerY);
+
+				//si le pixel concerné fait bien partie de l'image
+				if(coordX<ip.getWidth() && coordX>=0 && coordY<ip.getHeight() && coordY>=0){
+
+					//on récupère la valeur du pixel
+					int value = ip.getPixel(coordX, coordY);
+
+					//on met la valeur de ce pixel dans l'image
+					ipRotate.putPixel(i, j, value);
+				}
+			}
+		}
+		return ipRotate;
+	}
+}
--- src/rotation/RotationTest_.java
+++ src/rotation/RotationTest_.java
@@ -2,39 +2,15 @@ package rotation;
 import ij.ImagePlus;
 import ij.gui.ImageWindow;
 import ij.plugin.filter.PlugInFilter;
-import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
 
 
 public class RotationTest_ implements PlugInFilter {
-	/**
-	 * 
-	 * @param ip image to rotate
-	 * @param theta the angle to rotate the image
-	 * This function rotates a card with the equation of a line
-	 */
-	public static ImageProcessor rotate(ImageProcessor ip, int angle){
-		
-		ImageProcessor dst = new ByteProcessor(ip.getWidth(), ip.getHeight());
-		
-		dst = rotate(ip, 45);
-		return dst;
-	}
-	
-	/**
-	 * Find the angle of the line
-	 * @param a in ax+b
-	 * @param b in ax+b
-	 * @return the angle of the line
-	 */
-	public int getTheta(int a, int b){
-		return (int)Math.atan(b/a);
-	}
 
 	@Override
 	public void run(ImageProcessor arg0) {
-		ImageProcessor ipRot = rotate(arg0.duplicate(), 45);
-        ImagePlus impRot = new ImagePlus("kjderhfdfbvjd", ipRot);
+		ImageProcessor ipRot = Rotation.rotate(arg0, 45);
+        ImagePlus impRot = new ImagePlus("Apres rotation", ipRot);
         new ImageWindow(impRot);
 	}
 
